c46039024b5b86fbec4ab09646762cf1df0c64c1
Rahul Karadwal <rahulkaradwal2000@gmail.com>
2024-01-17 21:06:47 -0500

    fix: passed dynamic data in watched sumary

--- src/components/WatchedSummary.js
+++ src/components/WatchedSummary.js
@@ -1,26 +1,36 @@
 import React from "react";
 
 function WatchedSummary({ watchedData }) {
-  console.log(watchedData);
+  const iRatingAvg =
+    watchedData.reduce((curr, movie) => curr + movie.imdbRating, 0) /
+    watchedData.length;
+  const uRatingAvg =
+    watchedData.reduce((curr, movie) => curr + movie.userRating, 0) /
+    watchedData.length;
+
+  const watchedTimeAvg =
+    watchedData.reduce((curr, movie) => curr + movie.runtime, 0) /
+    watchedData.length;
+
   return (
     <div className="summary">
       <h2>Movies you watched</h2>
       <div>
         <p>
           <span>#️⃣</span>
-          <span>5 movies</span>
+          <span>{watchedData.length} movies</span>
         </p>
         <p>
           <span>⭐️</span>
-          <span>8</span>
+          <span>{iRatingAvg || 0}</span>
         </p>
         <p>
           <span>🌟</span>
-          <span>8</span>
+          <span>{uRatingAvg || 0}</span>
         </p>
         <p>
           <span>⏳</span>
-          <span>59 min</span>
+          <span>{watchedTimeAvg || 0} min</span>
         </p>
       </div>
     </div>
